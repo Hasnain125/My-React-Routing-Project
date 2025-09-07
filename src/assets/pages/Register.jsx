8cd9e924c54025d17ff12d63b69b5326b8161f4d
Hasnain125 <hunaianqadri1122@gmail.com>
2025-09-07 13:46:53 +0500

    Hasnain React Routing Project

--- src/assets/pages/Register.jsx
+++ src/assets/pages/Register.jsx
@@ -28,7 +28,7 @@ export default function Register() {
               name="name"
               type="text"
               className="w-full mt-1 px-4 py-2 rounded-xl border border-gray-300 focus:ring-2 focus:ring-blue-200 focus:outline-none"
-              placeholder="John Doe"
+              placeholder="Hasnian Saleem"
               required
             />
           </div>
